7aafbe05c0ea55571c299e404b70d5ad06416542
akalaj <akalaj@redux2.redux2>
2022-05-25 15:55:48 -0700

    Fixed API. Updated 'apiServer.js' to listen on IPv4 channels.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
+export default function App() {
+  const baseUrl = process.env.REACT_APP_API_URL;
+  console.log(baseUrl + "!");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Fuck IPv6!</h1>
     </div>
   );
 }
-
-export default App;
