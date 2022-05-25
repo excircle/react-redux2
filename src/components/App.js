a80115e7513ba16eb33136a8632f45297576d003
akalaj <akalaj@redux2.redux2>
2022-05-25 16:03:34 -0700

    Initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App() {
-  const baseUrl = process.env.REACT_APP_API_URL;
-  console.log(baseUrl + "!");
   return (
     <div>
       <h1>Fuck IPv6!</h1>
