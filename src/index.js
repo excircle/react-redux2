72ad96d454ba5b15af77f66e31eb47a10b571d25
akalaj <akalaj@redux2.redux2>
2022-05-25 17:30:45 -0700

    Functional Redux flow (without API)

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider as ReduxProvider } from 'react-redux';
+import configureStore from './redux/configureStore';
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./components/App";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const store = configureStore();
 
 root.render(
-  <Router>
-    <App tab="home" />
-  </Router>
+  <ReduxProvider store={store}>
+    <Router>
+      <App tab="home" />
+    </Router>
+  </ReduxProvider>
 );
