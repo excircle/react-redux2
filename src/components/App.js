38a9757ef5ede1712b1f9632c4c72721635a7a16
akalaj <akalaj@redux2.redux2>
2022-05-27 12:12:07 -0700

    Empty manage course page

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { Route, Routes, Navigate } from "react-router-dom";
 import Header from "./common/header";
 import HomePage from './home/HomePage';
 import CoursesPage from './courses/CoursesPage';
+import ManageCoursePage from './courses/ManageCoursePage';
 import AboutPage from './about/AboutPage';
 import NotFoundPage from './NotFoundPage';
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -15,6 +16,8 @@ function App() {
         <Route path="/" exact element={<HomePage />} />
         <Route path="/courses" element={<CoursesPage />} />
         <Route path="/about" element={<AboutPage />} />
+        <Route path="/course/:slug" element={<ManageCoursePage />} />
+        <Route path="/course/" element={<ManageCoursePage />} />
         <Route path="*" element={<NotFoundPage />} />
         <Route path="/about-page" element={<Navigate replace to="/about" />} />
       </Routes>
