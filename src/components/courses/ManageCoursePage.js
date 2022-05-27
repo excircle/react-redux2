6c1c62e5e396a25b42fed3dc1953be758e4ecd48
akalaj <akalaj@redux2.redux2>
2022-05-27 12:57:49 -0700

    Added common components. Confirmed newCourse is being derived from MockData.js

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { loadCourses } from "../../redux/actions/courseActions";
 import { loadAuthors } from "../../redux/actions/authorActions";
 import PropTypes from "prop-types";
+import CourseForm from './CourseForm';
+import { newCourse } from "../common/MockData";
 
-class ManageCoursePage extends React.Component {
-    componentDidMount() {
-        const { courses, authors, loadAuthors, loadCourses } = this.props;
+function ManageCoursePage({ courses, authors, loadAuthors, loadCourses, ...props }) {
+    const [course, setCourse] = useState({ ...props.course });
+    const [errors, setErrors] = useState({});
 
+    useEffect(() => {
         if (courses.length === 0) {
             loadCourses().catch(error => {
                 alert("Loading courses failed" + error);
@@ -19,18 +22,17 @@ class ManageCoursePage extends React.Component {
                 alert("Loading authors failed" + error);
             });
         }
-    }
+    }, []);
 
-    render() {
-        return (
-            <>
-                <h2>Manage Course</h2>
-            </>
-        );
-    }
+    return (
+        <>
+            <CourseForm course={course} errors={errors} authors={authors} />
+        </>
+    );
 }
 
 ManageCoursePage.propTypes = {
+    course: PropTypes.object.isRequired,
     authors: PropTypes.array.isRequired,
     courses: PropTypes.array.isRequired,
     loadCourses: PropTypes.func.isRequired,
@@ -39,6 +41,7 @@ ManageCoursePage.propTypes = {
 
 function mapStateToProps(state) {
     return {
+        course: newCourse,
         courses: state.courses,
         authors: state.authors
     }
